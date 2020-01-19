311d980c68f1020e27c2b76659636480be4344bf
Dimas_Arya_Putra <dimasarya021@gmail.com>
2020-01-19 09:39:45 +0700

    file pertama saya

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project UAS Bahasa Pemrograman
+silakan tuliskan langkah-langkah disertai screenshotnya
+ubah codingan yang sudah dikasih
+ubah baseapp.py dan main.py
---
+++ main.py
@@ -0,0 +1,20 @@
+from core.baseapp import BaseApp
+
+class MainApp(BaseApp):
+    def __init__(self):
+        self.books = []
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+
+class view(Viewbook):
+    def__init__(self)
+       vbook = ViewBook(self.book)
+       bBook.list()
+
+if__name__=="__main__":
+    app = MainApp()
+    app.run()
+
